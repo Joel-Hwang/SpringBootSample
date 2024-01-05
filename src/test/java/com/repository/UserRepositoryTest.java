49aab128beb491decc2f0a0bc265aded1f818b2f
Joel-Hwang <joel.c.hwang@gmail.com>
2024-01-04 22:42:05 -0600

    lazy loading testcase

--- src/test/java/com/repository/UserRepositoryTest.java
+++ src/test/java/com/repository/UserRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import com.model.AppUser;
+import com.model.Department;
 
 @DataJpaTest
 public class UserRepositoryTest {
@@ -17,6 +18,14 @@ public class UserRepositoryTest {
     public void testModelRepository() {
         AppUser myModel = new AppUser("test","test@email.com");
         myModel.setId(2L);
+        Department dept = new Department();
+        dept.setId(1L);
+        dept.setName("Test Team");
+        dept.setManager(2L);
+        dept.setParent(null);
+        dept.setUseYn('Y');
+
+        myModel.setDepartment(dept);
 
         // 저장
         AppUser savedModel = repository.save(myModel);
@@ -24,6 +33,7 @@ public class UserRepositoryTest {
         // 조회
         AppUser retrievedModel = repository.findById(savedModel.getId()).orElse(null);
 
+        Department dep = retrievedModel.getDepartment();
         // 예상 결과와 실제 결과 비교
         assertEquals(myModel.getEmail(), retrievedModel.getEmail());
         assertEquals(myModel.getUsername(), retrievedModel.getUsername());
